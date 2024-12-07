4a46bf6d1243a5843b61dbf75d9b3957b8ca9b39
Vinay <rathoreyuvrajsingh38@gmail.com>
2024-12-07 21:51:23 +0530

    first commt

---
+++ server/index.js
@@ -0,0 +1,63 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./routes/User.js";
+
+dotenv.config();
+
+const app = express();
+
+// CORS Configuration
+const corsOptions = {
+  origin: 'http://localhost:3000',  // mu frontend URL
+  methods: 'GET,POST,PUT,DELETE',   // HTTP methods your backend allows
+  allowedHeaders: 'Content-Type,Authorization',  // Headers allowed from the frontend
+};
+
+// Apply CORS middleware with configuration
+app.use(cors(corsOptions));
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/user/", UserRoutes);
+
+// Error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
+
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello developers from GFG",
+  });
+});
+
+const connectDB = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("Connected to Mongo DB"))
+    .catch((err) => {
+      console.error("Failed to connect with Mongo");
+      console.error(err);
+    });
+};
+
+const startServer = async () => {
+  try {
+    connectDB();
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
